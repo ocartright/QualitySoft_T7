10d495d7da4741e11e2151cf91733bcc3fbb2a5a
Justus <justusflerlage@taista.net>
2014-11-13 12:03:25 -0800

    added fullname support

--- source/skelton.cpp
+++ source/skelton.cpp
@@ -10,10 +10,13 @@ private:
    string firstName, lastName, streetAddress, email, phone, notes;
 public:
     Contact();
+    Contact(const string& fullName, const string& streetAddress, const string& email, 
+    		const string& phone, const string& notes);
     Contact(const string& firstName, const string& lastName, const string& streetAddress, const string& email, 
     		const string& phone, const string& notes);
 
     //functions
+    string getFullName() const;
     const string& getFirstName() const;
     const string& getLastName() const;
     const string& getStreetAddress() const;
@@ -21,12 +24,13 @@ public:
     const string& getPhone() const;
     const string& getNotes() const;
 
-    void setFirstName(const string &firstName);
-    void setLastName(const string &lastName);
-    void setStreetAddress(const string &streetAddress);
-    void setEmail(const string &email);
-    void setPhone(const string &phone);
-    void setNotes(const string &notes);
+	void setFullName(const string& fullName);
+    void setFirstName(const string& firstName);
+    void setLastName(const string& lastName);
+    void setStreetAddress(const string& streetAddress);
+    void setEmail(const string& email);
+    void setPhone(const string& phone);
+    void setNotes(const string& notes);
 }
 
 // manages all contacts by creating, modifying, deleting and granting access to them
@@ -38,12 +42,17 @@ public:
 	//creates new contact and returns a reference
 	Contact& add(const Contact& contact=Contact());
 	
-	//removes a contact by given element ( e.g. id )
+	//removes a contact by given element ( e.g. id ) or fullname
 	void remove(std::size_t element);
+	void remove(const string & fullName);
 	
 	//get a contact by given element ( e.g. id )
-	Contact& getContact();
-	const Contact& getContact() const;
+	Contact& getContact(std::size_t element);
+	const Contact& getContact(std::size_t element) const;
+	
+	//get a contact by given fullname
+	Contact& getContact(const string & fullName);
+	const Contact& getContact(const string & fullName) const;
 };
 
 //console interface
