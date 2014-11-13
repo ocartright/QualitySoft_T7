60aa43dcee122b5cd29cf1874e7a6e4e9dc17665
andyalam <andyalam@hotmail.com>
2014-11-12 16:15:52 -0800

    Create source.cpp
    .

---
+++ source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>;
+using namespace std;
+
+class Storage
+{
+   private:
+      string firstname, lastname, street, email, phone, notes;
+      string Person[6] = { firstname, lastname, street, email, phone, notes };
+
+   public:
+      Storage();
+      ~Storage();
+      
+      void storeFirst(string 
+
+};
+
+class User
+{
+   private:
+
+   public:
+
+};
+
+int main()
+{
+
+   cin.get();
+   return 0;
+}
+
