ba4a88fce86386e0ea5fddcb40eb47e21f2b607f
Orion Cartright <omagpiong@users.noreply.github.com>
2014-11-12 17:15:24 -0800

    source.cpp

--- source.cpp
+++ source.cpp
@@ -1,32 +1,53 @@
-#include <iostream>;
+//Andy
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Storage
-{
-   private:
-      string firstname, lastname, street, email, phone, notes;
-      string Person[6] = { firstname, lastname, street, email, phone, notes };
+class Contact {
+private:
+   //contact information
+   string firstName, lastName, streetAddress, email, phone, notes;
+public:
+    Contact();
 
-   public:
-      Storage();
-      ~Storage();
-      
-      void storeFirst(string 
+    //functions
+    const string& getFirstName() const;
+    const string& getLastName() const;
+    const string& getStreetAddress() const;
+    const string& getEmail() const;
+    const string& getPhone() const;
+    const string& getNotes() const;
 
-};
-
-class User
-{
-   private:
-
-   public:
-
-};
-
-int main()
-{
-
-   cin.get();
-   return 0;
+    void setFirstName(const string &firstName);
+    void setLastName(const string &lastName);
+    void setStreetAddress(const string &streetAddress);
+    void setEmail(const string &email);
+    void setPhone(const string &phone);
+    void setNotes(const string &notes);
 }
 
+class ContactManager {
+private:
+    vector<Contact> contacts;
+public:
+    //functions
+    /* create a new Contact object */
+    void makeNewContact();
+    /* access and modify a Contact object */
+    const Contact& getContact() const;
+    /* access and modify a Contact object */
+    Contact& getContact();
+};
+
+class Console {
+private:
+
+public:
+
+};
+
+int main() {
+
+
+    return 0;
+}
