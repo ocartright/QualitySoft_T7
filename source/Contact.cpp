5af4a1dda28eb3c943c28653817c13d49fff7a0e
andyalam <andyalam@hotmail.com>
2014-11-19 09:27:37 -0800

    Update Contact.cpp

--- source/Contact.cpp
+++ source/Contact.cpp
@@ -1,35 +1,122 @@
-#pragma once
+#include "Contact.hpp"
 
-#include <string>
 
-//represents a contact which its information
-class Contact {
-private:
-   std::string firstName, lastName, streetAddress, email, phone, notes;
-   
-public:
-	//constructors
-	Contact();
-	Contact(const std::string& fullName, const std::string& streetAddress, const std::string& email, 
-			const std::string& phone, const std::string& notes);
-	Contact(const std::string& firstName, const std::string& lastName, const std::string& streetAddress, const std::string& email, 
-			const std::string& phone, const std::string& notes);
+//constructors
+Contact::Contact() :
+firstName(""),
+lastName(""),
+streetAddress(""),
+email(""),
+phone(""),
+notes("")
+{
+ 
+}
 
-	//getters for private members
-	std::string getFullName() const;
-	const std::string& getFirstName() const;
-	const std::string& getLastName() const;
-	const std::string& getStreetAddress() const;
-	const std::string& getEmail() const;
-	const std::string& getPhone() const;
-	const std::string& getNotes() const;
-	
-	//setters for private members
-	void setFullName(const std::string& fullName);
-	void setFirstName(const std::string& firstName);
-	void setLastName(const std::string& lastName);
-	void setStreetAddress(const std::string& streetAddress);
-	void setEmail(const std::string& email);
-	void setPhone(const std::string& phone);
-	void setNotes(const std::string& notes);
-};
+Contact::Contact(const std::string& fullName, const std::string& streetAddress, const std::string& email,
+   const std::string& phone, const std::string& notes) :
+   streetAddress(streetAddress),
+   email(email),
+   phone(phone),
+   notes(notes)
+{
+   this->setFullName(fullName);
+}
+
+Contact::Contact(const std::string& firstName, const std::string& lastName, const std::string& streetAddress, const std::string& email,
+   const std::string& phone, const std::string& notes) :
+   firstName(firstName),
+   lastName(lastName),
+   streetAddress(streetAddress),
+   email(email),
+   phone(phone),
+   notes(notes)
+{
+  
+}
+
+
+
+//getters
+std::string Contact::getFullName() const
+{
+   return this->firstName +" "+this->lastName;
+}
+
+const std::string& Contact::getFirstName() const
+{
+   return this->firstName;
+}
+
+const std::string& Contact::getLastName() const
+{
+   return this->lastName;
+}
+
+const std::string& Contact::getStreetAddress() const
+{
+   return this->streetAddress;
+}
+
+const std::string& Contact::getEmail() const
+{
+   return this->email;
+}
+
+const std::string& Contact::getPhone() const
+{
+   return this->phone;
+}
+
+const std::string& Contact::getNotes() const
+{
+   return this->notes;
+}
+
+
+
+//setters
+void Contact::setFullName(const std::string& fullName)
+{
+   std::size_t i = fullName.find(' ');
+
+   if (i != std::string::npos)
+   {
+      this->firstName = fullName.substr(0, i);
+      this->lastName = fullName.substr(i, fullName.size() - i);
+   }
+   else
+   {
+      this->firstName = fullName;
+   }
+}
+
+void Contact::setFirstName(const std::string& firstName)
+{
+   this->firstName = firstName;
+}
+
+void Contact::setLastName(const std::string& lastName)
+{
+   this->lastName = lastName;
+}
+
+void Contact::setStreetAddress(const std::string& streetAddress)
+{
+   this->streetAddress = streetAddress;
+}
+
+void Contact::setEmail(const std::string& email)
+{
+   this->email = email;
+}
+
+void Contact::setPhone(const std::string& phone)
+{
+   this->phone = phone;
+}
+
+void Contact::setNotes(const std::string& notes)
+{
+   this->notes = notes;
+}
