e8b06fc2acd479e95a8f08df1650b22c01e828f5
Justus <justusflerlage@taista.net>
2014-11-26 09:11:59 -0800

    fltk fix

---
+++ build/CMakeFiles/application.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/application.dir/home/justus/Desktop/qualitysoft/source/Application.cpp.o"
+  "CMakeFiles/application.dir/home/justus/Desktop/qualitysoft/source/Contact.cpp.o"
+  "CMakeFiles/application.dir/home/justus/Desktop/qualitysoft/source/Main.cpp.o"
+  "CMakeFiles/application.dir/home/justus/Desktop/qualitysoft/source/ContactManager.cpp.o"
+  "CMakeFiles/application.dir/home/justus/Desktop/qualitysoft/source/Console.cpp.o"
+  "CMakeFiles/application.dir/home/justus/Desktop/qualitysoft/source/GraphicaUserInterface.cpp.o"
+  "application.pdb"
+  "application"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
