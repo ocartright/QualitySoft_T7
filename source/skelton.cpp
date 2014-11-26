b6565e141879f38dc8d862f42a8e21fb45087f63
Orion Cartright <omagpiong@users.noreply.github.com>
2014-11-25 21:42:50 -0800

    Latest Version

--- source/skelton.cpp
+++ source/skelton.cpp
@@ -2,14 +2,14 @@
  * This file was created and edited by Andy Alameddine, Justus Flerlage, and Orion Vuong.
  * There is not one author of any class or method in this file. We worked together in a couple of meetings to create the skeleton.
  * Program successfully compiled and run.
- * Final edits and clean-ups done by Justus Flerlage.
+ * Final edits and clean-ups done by Justus Flerlage and Orion Vuong.
  */
 
 #include <iostream>
 #include <vector>
 #include <string>
 
-//gui library
+//gui library (used in GUI version only and must be an included library to compile properly)
 #include <FL/Fl_Widget.H>
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
@@ -20,125 +20,151 @@
 #include <FL/Fl_Pack.H>
 #include <FL/Fl_Input.H>
 
+//class pre-declarations so that compiler knows these types exist in the program
 class Application;
 class Contact;
 class ContactManager;
 class Console;
 
-//represents a contact which stores its information
+/* Represents a single contact and hold all the information about that contact. */
 class Contact {
 private:
+   //general information about the contact that the user would be interested in
    std::string firstName, lastName, address, email, phone, notes;
 
 public:
-	//constructors
+	//class constructors for the Contact class
 	Contact();
+	//the object can be instantiated and give one variable, the contact's full name
 	Contact(const std::string& fullName, const std::string& address, const std::string& email,
 			const std::string& phone, const std::string& notes);
+	//the object can be instantiated and give two variables, the contact's first name and the contact's last name
 	Contact(const std::string& firstName, const std::string& lastName, const std::string& address, const std::string& email,
 			const std::string& phone, const std::string& notes);
-
-	//getters for private members
+	
+	//all the getters that return the contact's credentials
+	/* Returns the full name of the contact as one string variable. */
 	std::string getFullName() const;
+	/* Returns the first name of the contact as a string. */
 	const std::string& getFirstName() const;
+	/* Returns the last name of the contact as a string. */
 	const std::string& getLastName() const;
+	/* Returns the contact's address as a string. */
 	const std::string& getAddress() const;
+	/* Returns the contact's email address as a string. */
 	const std::string& getEmail() const;
+	/* Returns the contact's phone number as a string. */
 	const std::string& getPhone() const;
+	/* Returns a special notes about the contact as a string. */
 	const std::string& getNotes() const;
 
-	//setters for private members
+	//all the setters that store the contact's credentials
+	/* Sets the full name of the contact as one string variable. */
 	void setFullName(const std::string& fullName);
+	/* Sets the first name of the contact as a string. */
 	void setFirstName(const std::string& firstName);
+	/* Sets the last name of the contact as a string. */
 	void setLastName(const std::string& lastName);
+	/* Sets the contact's address as a string. */
 	void setAddress(const std::string& address);
+	/* Sets the contact's email address as a string. */
 	void setEmail(const std::string& email);
+	/* Sets the contact's phone number as a string. */
 	void setPhone(const std::string& phone);
+	/* Sets a special notes about the contact as a string. */
 	void setNotes(const std::string& notes);
 };
 
-// manages all contacts by creating, modifying, deleting and granting access to them
+/* Manages all Contact objects. Creates new Contact objects and deletes and/or grants access to existing Contact objects. */
 class ContactManager {
 private:
+	//a vector that holds all Contact objects
 	std::vector<Contact> contacts;
 	
 public:
-	// returns amount of contacts
+	/* Returns the size of the 'contacts' vector, which represents the number of stored contacts. */
 	size_t getSize() const;
 
-	//creates new contact and returns a reference
+	/* Adds a new Contact object that was created either in the Console class or GUI class to the 'contacts' vector. */
 	void add(const Contact& contact);
 	
-	//removes a contact by given element ( e.g. id ), object or fullname
+	//overloaded method: removes an existing contact using a given element
+	/* Removes an existing contact using the position of the Contact in the 'contacts' vector. */
 	void remove(std::size_t element);
+	/* Removes an existing contact using a reference to a Contact object. */
 	void remove(const Contact& contact);
+	/* Removes an existing contact using a reference to the contact's full name. */
 	void remove(const std::string& fullName);
 	
-	//get a contact by given element ( e.g. id )
+	//overloaded method: returns a pointer to a Contact object using a given element
+	/* Returns a reference to a Contact object using the position of the Contact in the 'contacts' vector. Contact object may be modified. */
 	Contact* get(std::size_t element);
+	/* Returns a pointer to a Contact object using the position of the Contact in the 'contacts' vector.  */
 	const Contact* get(std::size_t element) const;
-	
-	//get a contact by given fullname
+	/* Returns a pointer to a Contact object using a reference to the contact's full name. May modify the reference to the object or the object itself. */
 	Contact* get(const std::string& fullName);
+	/* Returns a reference to a Contact object using a reference to the contact's full name. May not be modified at all. */
 	const Contact* get(const std::string& fullName) const;
 	
-	//methods for saving and writing all contacts
+	/* Writes all Contact objects in the 'contacts' vector into a specified file. */
 	void save(const std::string& fileName) const;
+	/* Retrieves all Contact objects in the 'contacts' vector from a specified file. */
 	void load(const std::string& fileName);
 };
 
-//console interface
+/* User Interface that manages console input from the user and displays output in the console. */
 class Console {
 private:
+	//used when instantiating a Console object. Specifies that an Application class will be using the console interface instead of a GUI.
 	Application & application;
 public:
-	//constructor
+	//the object can be instantiated and give one variable, the reference to the application that runs the program
 	Console(Application & application);
 
-	//if the console interface is used in the program, this method will be used to run the program
+	/* Used by the 'application' object to run the program if a console interface is being used in the program. */
 	void run();
 	
-	//get a contact by asking the user
+	/* Returns a pointer to a Contact object, which is found using the user's input in the console. */
 	Contact* getContact() ;
+	/* Returns a pointer to a Contact object, which is found using the user's input in the console. */
 	const Contact* getContact() const;
 	
-	//show a contact by getting the contact with console input, itself, element or fullName
+	/* Shows a contact's credentials. */
 	void showContact() const;
+	/* Shows a specific contact's credentials that were retrieved by the getContact() method. */
 	void showContact(const Contact& contact) const;
 	
-	//show all contacts
+	/* Shows credentials of all contacts in the 'contacts' vector. */
 	void showContacts() const;
 	
-	//contact by getting the contact with console input, itself, element or fullName
+	/* Removes a Contact object from the 'contacts' vector */
 	void removeContact();
+	/* Removes a specific Contact object, retrieved using the getContact() method, from the 'contacts' vector. */
 	void removeContact(Contact & contact);
 	
-	//modify a contact by getting the contact with console input, itself, element or fullName
-	void modifyContact();
-	void modifyContact(Contact & contact);
-	
-	//add a contact
+	/* Adds a new Contact object to the 'contacts' vector */
 	void addContact();
 };
 
-//gui
+/* User Interface (Graphical) that manages input from a windowed canvas and displays output on the canvas. */
 class GraphicalUserInterface {
 private:
+	//used when instantiating a GraphicalUserInterface object. Specifies that an Application class will be using a GUI instead of a console interface.
 	Application& application;
 
-	//callbacks
+	/* Callback methods required by Widgets below */
 	static void onSelect(Fl_Widget* widget, void* p);
 	static void onAdd(Fl_Widget* widget, void* p);
 	static void onSave(Fl_Widget* widget, void* p);
 	static void onRemove(Fl_Widget* widget, void* p);
 
-	//current selected contact
+	//reference to the user's currently selected contact
 	Contact* selectedContact;
 
-	//browser
+	//browser updater
 	void updateBrowser();
 
-	//widgets
+	//widgets used for input
     Fl_Window window;
 	Fl_Select_Browser browser;
 	Fl_Button add;
@@ -152,28 +178,30 @@ private:
 	Fl_Input notes;
 
 public:
-	//constructor
+	//the object can be instantiated and give one variable, the reference to the application that runs the program
 	GraphicalUserInterface(Application& application);
 
-	// main method
+	/* Used by the 'application' object to run the program if a GUI is being used in the program. */
 	void run();
 };
 
-//represents the program as an object
+/* Represents the ContactList application as an object that uses either the Console interface or GUI. */
 class Application {
 private:
+	//the ContactManager object that will manage the contact list throughout the program
 	ContactManager contactManager;
 
 public:
-	//getter for contactManager
+	/* Returns a reference to the application's contactManager object. */
 	ContactManager& getContactManager();
 	const ContactManager& getContactManager() const;
 
-	//main method
+	/* Used to run the ContactList application itself. Calls the run() method from either Console class or GraphicalUserInterface class */
 	int main(int size, char** arguments);
 };
 
 int main (int size, char** arguments)
 {
+	//calls the main() method in class Application, which then calls the run() method to run the program (either from the Console class or GraphicalUserInterface class)
 	return Application().main(size, arguments);
 }
