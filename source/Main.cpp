2f99012880ee6d4801757fa19f0350abea70e97c
Orion Cartright <omagpiong@users.noreply.github.com>
2014-11-29 14:14:57 -0800

    comment update

--- source/Main.cpp
+++ source/Main.cpp
@@ -2,5 +2,6 @@
 
 int main (int size, char** arguments)
 {
+	//calls the main() method in class Application, which then calls the run() method to run the program (either from the Console class or GraphicalUserInterface class)
 	return Application().main(size, arguments);
 }
