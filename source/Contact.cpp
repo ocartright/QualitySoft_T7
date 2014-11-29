a09f2061b4fa88a4ea7c19320bac66a1cf7bfd28
Orion Cartright <omagpiong@users.noreply.github.com>
2014-11-29 13:59:22 -0800

    comment update

--- source/Contact.cpp
+++ source/Contact.cpp
@@ -1,6 +1,7 @@
 #include "Contact.hpp"
 
-//constructors
+/* Represents a single contact and hold all the information about that contact. */
+//class constructors for the Contact class
 Contact::Contact() :
 	firstName(""),
 	lastName(""),
@@ -10,6 +11,7 @@ Contact::Contact() :
 	notes("")
 {
 }
+//the object can be instantiated and give one variable, the contact's full name
 Contact::Contact(const std::string& fullName, const std::string& address, const std::string& email,
    const std::string& phone, const std::string& notes) :
    address(address),
@@ -19,6 +21,7 @@ Contact::Contact(const std::string& fullName, const std::string& address, const 
 {
    this->setFullName(fullName);
 }
+//the object can be instantiated and give two variables, the contact's first name and the contact's last name
 Contact::Contact(const std::string& firstName, const std::string& lastName, const std::string& address, const std::string& email,
    const std::string& phone, const std::string& notes) :
    firstName(firstName),
@@ -30,39 +33,45 @@ Contact::Contact(const std::string& firstName, const std::string& lastName, cons
 {
 }
 
-//getters
+//all the getters that return the contact's credentials
+/* Returns the full name of the contact as one string variable. */
 std::string Contact::getFullName() const
 {
    return this->firstName +" "+this->lastName;
 }
-
+/* Returns the first name of the contact as a string. */
 const std::string& Contact::getFirstName() const
 {
    return this->firstName;
 }
-
+/* Returns the last name of the contact as a string. */
 const std::string& Contact::getLastName() const
 {
    return this->lastName;
 }
+/* Returns the contact's address as a string. */
 const std::string& Contact::getAddress() const
 {
    return this->address;
 }
+/* Returns the contact's email address as a string. */
 const std::string& Contact::getEmail() const
 {
    return this->email;
 }
+/* Returns the contact's phone number as a string. */
 const std::string& Contact::getPhone() const
 {
    return this->phone;
 }
+/* Returns a special notes about the contact as a string. */
 const std::string& Contact::getNotes() const
 {
    return this->notes;
 }
 
-//setters
+//all the setters that store the contact's credentials
+/* Sets the full name of the contact as one string variable. */
 void Contact::setFullName(const std::string& fullName)
 {
    std::size_t i = fullName.find(' ');
@@ -77,27 +86,32 @@ void Contact::setFullName(const std::string& fullName)
       this->firstName = fullName;
    }
 }
-
+/* Sets the first name of the contact as a string. */
 void Contact::setFirstName(const std::string& firstName)
 {
    this->firstName = firstName;
 }
+/* Sets the last name of the contact as a string. */
 void Contact::setLastName(const std::string& lastName)
 {
    this->lastName = lastName;
 }
+/* Sets the contact's address as a string. */
 void Contact::setAddress(const std::string& address)
 {
    this->address = address;
 }
+/* Sets the contact's email address as a string. */
 void Contact::setEmail(const std::string& email)
 {
    this->email = email;
 }
+/* Sets the contact's phone number as a string. */
 void Contact::setPhone(const std::string& phone)
 {
    this->phone = phone;
 }
+/* Sets a special note about the contact as a string. */
 void Contact::setNotes(const std::string& notes)
 {
    this->notes = notes;
