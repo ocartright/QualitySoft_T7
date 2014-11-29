af0089ef3bec67d6081f1d2870132acb23f7c7de
Orion Cartright <omagpiong@users.noreply.github.com>
2014-11-29 13:50:00 -0800

    comment update

--- source/Console.hpp
+++ source/Console.hpp
@@ -6,36 +6,40 @@
 class Application;
 class Contact;
 
-//console interface
+/* User Interface that manages console input from the user and displays output in the console. */
 class Console {
 private:
+	//used when instantiating a Console object. Specifies that an Application class will be using the console interface instead of a GUI.
 	Application & application;
 public:
-	//constructor
+	//the object can be instantiated and give one variable, the reference to the application that runs the program
 	Console(Application & application);
 
-	//if the console interface is used in the program, this method will be used to run the program
+	/* Used by the 'application' object to run the program if a console interface is being used in the program. */
 	void run();
 	
-	//get a contact by asking the user
+	/* Returns a pointer to a Contact object, which is found using the user's input in the console. */
 	Contact* getContact() ;
+	/* Returns a pointer to a Contact object, which is found using the user's input in the console. */
 	const Contact* getContact() const;
 	
-	//show a contact by getting the contact with console input, itself, element or fullName
+	/* Shows a contact's credentials. */
 	void showContact() const;
+	/* Shows a specific contact's credentials that were retrieved by the getContact() method. */
 	void showContact(const Contact& contact) const;
 	
-	//show all contacts
+	/* Shows credentials of all contacts in the 'contacts' vector. */
 	void showContacts() const;
 	
-	//contact by getting the contact with console input, itself, element or fullName
+	/*//modify a contact by getting the contact with console input, itself, element or fullName
+	void modifyContact();
+	void modifyContact(Contact & contact);*/
+	
+	/* Removes a Contact object from the 'contacts' vector */
 	void removeContact();
+	/* Removes a specific Contact object, retrieved using the getContact() method, from the 'contacts' vector. */
 	void removeContact(Contact & contact);
 	
-	//modify a contact by getting the contact with console input, itself, element or fullName
-	void modifyContact();
-	void modifyContact(Contact & contact);
-	
-	//add a contact
+	/* Adds a new Contact object to the 'contacts' vector */
 	void addContact();
 };
